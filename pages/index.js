343a02b8e7f688e31ef572004badc69b3d2e9c41
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-03-19 17:25:16 +0530

    Using SWR to fetch data

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,22 @@ import useSWR from 'swr';
 
 import {getUsers} from "../actions/user";
 
+import useSwr from 'swr';
 
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Index = (props) =>{
  
+const { data, error } = useSwr('/api/user', fetcher)
 
-
-
+  if (error) return <div>Failed to load users</div>
+  if (!data) return <div>Loading...</div>
   
-  return <code>{JSON.stringify(props.users, null, 2)}</code>;
+  return <code>{JSON.stringify(data, null, 2)}</code>;
 }
 
 
 
-export async function getStaticProps() {
-  // Call an external API endpoint to get posts.
-  // You can use any data fetching library
-  const users = await getUsers();
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-  return {
-    props: {
-      users
-    },
-  }
-}
 
 
 export default Index;
